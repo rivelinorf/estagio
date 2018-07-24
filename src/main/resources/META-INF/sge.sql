4d6aaf93cc05f73dec958cca9e20f8b43f334426
albanomatheus <albanomatheus16@gmail.com>
2018-07-24 11:43:20 -0300

    Tabela estado

--- src/main/resources/META-INF/sge.sql
+++ src/main/resources/META-INF/sge.sql
@@ -1,1 +1,12 @@
 create database sge;
+
+use sge;
+
+create table estado (
+        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(50) NOT NULL,
+        abv VARCHAR(10) 
+);
+
+insert into estado (nome, abv) values ("MINAS GERAIS", "MG");
+select * from estado;
