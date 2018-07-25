900e7d852cfa4646514bdce582e1d422eb5d2ad6
bsp982 <brunosantos.engproducao@gmail.com>
2018-07-25 14:00:22 -0300

    model:logradouro,tipoLogradouro

--- src/main/java/br/com/sonner/estagio/model/TipoLogradouro.java
+++ src/main/java/br/com/sonner/estagio/model/TipoLogradouro.java
@@ -3,21 +3,26 @@ package br.com.sonner.estagio.model;
 public class TipoLogradouro {
 	private long id;
 	private String nome;
-	
-	
-	
+
+	public TipoLogradouro(long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
 
 }
