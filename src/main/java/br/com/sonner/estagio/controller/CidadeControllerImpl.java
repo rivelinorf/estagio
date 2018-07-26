767f8c3fd325badd09495dc622bf9aa7be9e1482
albanomatheus <albanomatheus16@gmail.com>
2018-07-26 18:08:50 -0300

    dao/controller/servlet: inicio das implementacoes de cidade, falta testar o inserir

---
+++ src/main/java/br/com/sonner/estagio/controller/CidadeControllerImpl.java
@@ -0,0 +1,4 @@
+package br.com.sonner.estagio.controller;
+
+public class CidadeControllerImpl {
+}
---
+++ src/main/java/br/com/sonner/estagio/controller/api/CidadeController.java
@@ -0,0 +1,13 @@
+package br.com.sonner.estagio.controller.api;
+
+import br.com.sonner.estagio.model.Estado;
+
+import java.util.List;
+
+public interface EstadoController {
+    void save(Estado estado);
+    List<Estado> getAll();
+    Estado getOne(long id);
+    void update(Estado estado);
+    void delete(long id);
+}
