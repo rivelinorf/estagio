549ae48029cd29f3df33484794f30200e6f0dd91
albanomatheus <albanomatheus16@gmail.com>
2018-07-26 15:50:20 -0300

    pom, connection: adicionando comentarios

--- src/main/java/br/com/sonner/estagio/connection/Conn.java
+++ src/main/java/br/com/sonner/estagio/connection/Conn.java
@@ -1,5 +1,6 @@
 package br.com.sonner.estagio.connection;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -34,5 +35,10 @@ public class Conn {
         }
 
         return CONNECTION;
+    }
+
+
+    public static void main(String[] args) {
+        Connection connection = Conn.getConnection();
     }
 }
